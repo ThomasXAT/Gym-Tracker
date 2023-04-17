501ab16a039004708b90a74386e338cdbc4b82a7
Thomas JORGE <thomas.jorge.xat@gmail.com>
2023-04-17 15:13:35 +0200

    Création des routes principales

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    #[Route('/home', name: 'home')]
+    public function home(): Response
+    {
+        return $this->render('main/home/index.html.twig');
+    }
+
+    #[Route('/repertory', name: 'repertory')]
+    public function repertory(): Response
+    {
+        return $this->render('main/repertory/index.html.twig');
+    }
+
+    #[Route('/statistics', name: 'statistics')]
+    public function statistics(): Response
+    {
+        return $this->render('main/statistics/index.html.twig');
+    }
+
+    #[Route('/profile', name: 'profile')]
+    public function profile(): Response
+    {
+        return $this->render('main/profile/index.html.twig');
+    }
+}
