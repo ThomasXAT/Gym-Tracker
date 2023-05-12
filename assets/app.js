6b3f8386dd6fc93c70094021ecca4b78c80751ed
Thomas JORGE <thomas.jorge.xat@gmail.com>
2023-05-12 14:20:34 +0200

    Intégration de Font Awesome

--- assets/app.js
+++ assets/app.js
@@ -11,4 +11,7 @@ import './styles/app.scss';
 // start the Stimulus application
 import './bootstrap';
 
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
+
 require('bootstrap');
