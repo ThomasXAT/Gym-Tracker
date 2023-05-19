6255a9c8b0169d538fc08b2a7ab121842b15d968
Thomas JORGE <thomas.jorge.xat@gmail.com>
2023-05-19 22:17:25 +0200

    Ajout de la barre de recherche

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,11 +5,9 @@ namespace App\Controller;
 use App\Form\ProfileType;
 use App\Repository\AthleteRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use PHPUnit\Framework\Constraint\IsEmpty;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,18 +18,13 @@ class DefaultController extends AbstractController
     public function home(): Response
     {
         if (isset($_GET["search"])) {
-            throw new NotFoundHttpException('Search');
+            return $this->render('main/repertory/index.html.twig', [
+                'page' => 'repertory',
+                'search' => $_GET["search"],
+            ]);
         }
         return $this->render('main/home/index.html.twig', [
             'page' => 'home',
-        ]);
-    }
-
-    #[Route('/repertory', name: 'repertory')]
-    public function repertory(): Response
-    {
-        return $this->render('main/repertory/index.html.twig', [
-            'page' => 'repertory',
         ]);
     }
 
@@ -74,19 +67,6 @@ class DefaultController extends AbstractController
             }
             return $this->render('main/profile/index.html.twig', [
                 'page' => 'profile',
-                'athlete' => $athlete,
-            ]);
-        }
-        throw new NotFoundHttpException('Athlete not found. The requested user does not exist.');
-    }
-
-    #[Route('/@{username}/statistics', name: 'statistics')]
-    public function statistics(AthleteRepository $athleteRepository, String $username): Response
-    {
-        $athlete = $athleteRepository->findOneBy(['username' => $username]);
-        if ($athlete) {
-            return $this->render('main/statistics/index.html.twig', [
-                'page' => 'statistics',
                 'athlete' => $athlete,
             ]);
         }
